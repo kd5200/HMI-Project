07e04e576edd4f719d8775ca8a3746c1e0f7aadb
kd5200 <kareemdavis18@yahoo.com>
2024-03-09 03:56:18 -0500

    connected postgresDB and react frontend

--- HMI_django/HMI_django/settings.py
+++ HMI_django/HMI_django/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+# Remember to always register django App within the settings here.
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'HMIapp',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'HMI_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weather-django',
+        'USER': 'postgres',
+        'PASSWORD': 'KfauD202023$$',
+        'HOST': 'localhost',   
+        'PORT': '5432',        
     }
 }
 
