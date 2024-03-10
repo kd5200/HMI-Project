657ea12d122a8def6b085a913435cc85e4b8e60b
kd5200 <kareemdavis18@yahoo.com>
2024-03-09 21:11:38 -0500

    hitting API endpoints

--- HMI_django/HMIapp/models.py
+++ HMI_django/HMIapp/models.py
@@ -4,9 +4,7 @@ class weather(models.Model):
     condition = models.CharField(max_length=50)
     city = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.condition
-
+    
 
 # TBD
 # Create your models here.
--- HMI_django/HMIapp/views.py
+++ HMI_django/HMIapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import weather
+from .serializer import *
+from rest_framework.response import Response
+# from django.http import HttpResponse
+
+class ReactView(APIView):
+    def get(self, request):
+        output = [{"condition": output.condition,
+                   "city": output.city}
+                   for output in weather.objects.all()]
+        return Response(output)
+    
+    def post(self, request):
+        serializer = serializers.ModelSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=201)
 
 # Create your views here.
+
+
